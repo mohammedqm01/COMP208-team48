a2accad461d4fc4842e57fa7f6ed3741d7c83440
Kai Farrimond <93374198+KaiRF@users.noreply.github.com>
2023-04-03 14:48:23 +0100

    Add files via upload

---
+++ Study_Calendar/COMP208_Study_Calendar/EventCell.swift
@@ -0,0 +1,15 @@
+//
+//  EventCell.swift
+//  COMP208_Study_Calendar
+//
+//  Created by Kai Farrimond on 28/03/2023.
+//
+
+import UIKit
+
+class EventCell: UITableViewCell {
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var subjectLabel: UILabel!
+    @IBOutlet weak var secondaryLabel: UILabel!
+}
